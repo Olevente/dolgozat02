014a7c3067487713c39f7f17ca17a3dfc26ea9fe
Orosz Levente <oroszleventee1403@gmail.com>
2022-02-22 08:57:57 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,28 @@
+/*
+* File: App.java
+* Author: Orosz Levente
+* Copyright: 2022, Orosz Levente
+* Group: Szoft I N
+* Date: 2022-02-22
+* Github: https://github.com/Olevente/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+public class App {
+	
+	public static void main (String[] args) {
+		System.out.println("Orosz Levente, 2022.02.22, Szoft-I-N");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Magassag: ");
+		double magassag = sc.nextDouble();
+		System.out.print("Szog: ");
+		double szog = sc.nextDouble();
+		
+		double tavolsag = 
+		System.out.println("Tavolsag: " + tavolsag);
+	}
+}
+
--- tavolsag.java
+++
@@ -1,9 +0,0 @@
-
-
-public class névtelen {
-	
-	public static void main (String[] args) {
-		
-	}
-}
-
